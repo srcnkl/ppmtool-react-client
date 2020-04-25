261a07e1006a29bf63475148dc33ac6cbbbc9418
srcnkl <sercankaliu@gmail.com>
2020-04-25 17:20:45 +0300

    get project is completed on redux side

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react'
-import Projectitems from './Project/Projectitems';
-import Header from './Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
-import CreateProjectButton from './Project/CreateProjectButton';
+import React, { Component } from "react";
+import Projectitems from "./Project/Projectitems";
+import Header from "./Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            // Dashboard Component (Project Item included)
-            <div className="projects">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h1 className="display-4 text-center">Projects</h1>
-                        <br />
-                        <CreateProjectButton/>
-                        <br />
-                        <hr />
-                        <Projectitems/>
-                    </div>
-                </div>
+  componentDidMount(){
+      this.props.getProjects();
+  }
+  render() {
+    return (
+      // Dashboard Component (Project Item included)
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
+              <Projectitems />
             </div>
+          </div>
         </div>
-        );
-    }
+      </div>
+    );
+  }
 }
-export default Dashboard;
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+//state.project deki project bizim state içinde bulunan project tag imiz. index.js te tanımlı. error gibi.
+const mapStateToProps = (state) => ({
+  project: state.project
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
